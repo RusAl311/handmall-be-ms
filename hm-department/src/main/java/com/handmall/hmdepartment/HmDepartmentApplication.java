bbb9f9a118ad479cc302e545bc5aaad26f2bb628
rustam <rustamm.aliyev@gmail.com>
2023-04-15 22:41:59 +0400

    add department ms

---
+++ hm-department/src/main/java/com/handmall/hmdepartment/HmDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.handmall.hmdepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmDepartmentApplication.class, args);
+	}
+
+}
