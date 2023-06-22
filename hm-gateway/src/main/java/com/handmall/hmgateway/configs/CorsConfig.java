598e8b754bb5dea9bb971a42c70a03502a33f50b
rustam <rustamm.aliyev@gmail.com>
2023-06-22 11:26:44 +0400

    changed in cors configuration

--- hm-gateway/src/main/java/com/handmall/hmgateway/configs/CorsConfig.java
+++ hm-gateway/src/main/java/com/handmall/hmgateway/configs/CorsConfig.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig extends CorsConfiguration {
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    CorsWebFilter corsWebFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
@@ -20,7 +20,7 @@ public class CorsConfig extends CorsConfiguration {
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-        return source;
+        return new CorsWebFilter(source);
     }
 
 }
